bc94e94a2d6053f9b7ce289876a0e1a641c01612
Cedric <mrlee.truecoder@gmail.com>
2021-09-30 13:56:53 -0500

    Created IProductsRepository interface from Exercise 2.

---
+++ BestBuyBestPractices/IProductsRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BestBuyBestPractices
+{
+    public interface IProductsRepository
+    {
+        IEnumerable<Products> GetAllProducts();
+        void CreateProduct(string name, double price, int categoryID);
+    }
+}
